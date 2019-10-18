505b4f120e96d648b215e63f7ea34280dabe910b
AlfredYang1986 <yangyuanpig@gmail.com>
2019-10-18 18:17:59 +0800

    上数

--- dataPre/PhDataPre.R
+++ dataPre/PhDataPre.R
@@ -4,10 +4,10 @@ source("dataPre/dataPre.R")
 cal_J_data_pre <- function() {
     # 0. 数据准备
     # 0.1 Universe 文件
-    # cal_excel_data_to_parquet(
-    #     "/Users/alfredyang/Desktop/code/pharbers/Max-repo/tmp/2019年Universe更新维护1.0_190403.xlsx", 
-    #     "Universe2019", 
-    #     "hdfs://192.168.100.137:9000//Map-repo/2019年Universe更新维护1.0_190403/Universe2019")
+    cal_excel_data_to_parquet(
+        "/Users/alfredyang/Desktop/code/pharbers/Max-repo/tmp/2019年Universe更新维护1.0_190403.xlsx", 
+        "Universe2019", 
+        "hdfs://192.168.100.137:9000//Map-repo/2019年Universe更新维护1.0_190403/Universe2019")
     
     # 0.2 Mapping 文件
     cal_excel_data_to_parquet(
@@ -22,12 +22,12 @@ cal_J_data_pre <- function() {
         "hdfs://192.168.100.137:9000//Map-repo/CPA_VS_GYC_VS_PHA_VS_HH_0418")
     
     # 0.3 190814泰德-1701-1906检索 # 1701-1906
-    cal_excel_data_to_parquet(
+    cal_excel_large_data_to_parquet(
         "/Users/alfredyang/Desktop/code/pharbers/Max-repo/tmp/190814泰德-1701-1906检索.xlsx",
         "1701-1906数据", 
         "hdfs://192.168.100.137:9000//Map-repo/190814泰德-1701-1906检索/1701-1906")
     
-    # 0.3 universe # 1701-1906
+    # 0.4 universe # 1701-1906
     cal_excel_data_to_parquet(
         "/Users/alfredyang/Desktop/code/pharbers/Max-repo/tmp/universe.xlsx",
         "Sheet1", 
--- dataPre/dataPre.R
+++ dataPre/dataPre.R
@@ -5,3 +5,22 @@ cal_excel_data_to_parquet <- function(path, tab, dest) {
     mapDf <- createDataFrame(map)
     write.parquet(mapDf, dest)
 }
+
+cal_excel_large_data_to_parquet <- function(path, tab, dest, step = 10000L) {
+    map <- read.xlsx(path, sheet = tab)
+    d <- dim(map)
+    round <- d[1] / step
+    rleft <- d[1] %% step
+    index <- 0
+    while (index < round) {
+        print("round")
+        print(index)
+        tmp <- map[step * index + 1 : step * index + step, ]
+        tmpDf <- createDataFrame(tmp)
+        write.parquet(tmpDf, dest, mode = "append")
+        index <- index + 1
+    }
+    # tmpDf <- map[step * round : step * round + rleft, ]
+    # write.parquet(tmpDf, dest, mode = "append")
+    print("end")
+}
