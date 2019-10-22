ea42814df72215645c6f8893a26b7ddfc0d21a37
AlfredYang1986 <yangyuanpig@gmail.com>
2019-10-22 18:05:05 +0800

    语言设计理论很大差别

--- dataPre/dataPre.R
+++ dataPre/dataPre.R
@@ -18,12 +18,15 @@ cal_excel_large_data_to_parquet <- function(path, tab, dest, step = 10000L) {
         print(index)
         print(step * index + 1)
         print(step * index + step)
-        tmp <- map[step * index + 1 : step * index + step, ]
+        tmp <- map[(step * index + 1) : (step * index + step), ]
         tmpDf <- createDataFrame(tmp)
         write.parquet(tmpDf, dest, mode = "append")
         index <- index + 1
     }
-    tmpDf <- createDataFrame(map[step * round : step * round + rleft, ])
-    write.parquet(tmpDf, dest, mode = "append")
+    print(step * floor(round) + 1)
+    print(step * floor(round) + rleft)
+    tmpDf2 <- createDataFrame(map[(step * floor(round) + 1) : (step * floor(round) + rleft), ])
+    print(count(tmpDf2))
+    write.parquet(tmpDf2, dest, mode = "append")
     print("end")
 }
