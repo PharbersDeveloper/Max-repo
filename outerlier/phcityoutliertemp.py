f12617254d2177abb91c3736d0a5d5045f42aa93
AlfredYang1986 <yangyuanpig@gmail.com>
2020-01-13 14:14:00 +0800

    基本完成 全流程线上运行

--- outerlier/phcityoutliertemp.py
+++ outerlier/phcityoutliertemp.py
@@ -33,7 +33,7 @@ def max_outlier_city_loop_template(spark, df_EIA_res, df_seg_city, cities, num_o
         # print ct
         ot_seg = df_EIA_res_iter.where(df_EIA_res_iter.PANEL == 1) \
             .groupBy("Seg").sum("Est_DrugIncome_RMB") \
-            .orderBy("sum(Est_DrugIncome_RMB)").toPandas()["Seg"].to_numpy()[0]
+            .orderBy(func.desc("sum(Est_DrugIncome_RMB)")).toPandas()["Seg"].to_numpy()[0]
         # print ot_seg
 
         df_ot_city = df_EIA_res_iter.where(df_EIA_res_iter.PANEL == 1).select("Seg", "HOSP_ID").distinct()
@@ -115,4 +115,3 @@ def max_outlier_city_loop_template(spark, df_EIA_res, df_seg_city, cities, num_o
 
         # df_result.show()
         return df_result
-
--- outerlier/pyspOutlier.py
+++ outerlier/pyspOutlier.py
@@ -1,24 +1,19 @@
 # coding=utf-8
-import itertools
-import pandas as pd
 import numpy as np
 from cvxpy import *
-import sys
-import time
 
 from pyspark.sql import SparkSession
-from pyspark.sql import functions as func
 from pyspark.sql.types import *
 
 from phcityoutliertemp import max_outlier_city_loop_template
 from phimsshrjob import max_outlier_ims_shr_job
+from photfactor import max_outlier_factor
 from phpnljob import max_outlier_pnl_job
 from phreaddfjob import max_outlier_read_df
 from phpfjob import max_outlier_poi_job
 from phselectcity import max_outlier_select_city
 from phtmpmodify import max_outlier_tmp_mod
 from phunijoineia import max_outlier_eia_join_uni
-from phsegwoot import max_outlier_seg_wo_ot_old, max_outlier_seg_wo_ot_spark
 
 spark = SparkSession.builder \
     .master("yarn") \
@@ -34,6 +29,7 @@ spark.sparkContext.addPyFile("phimsshrjob.py")
 spark.sparkContext.addPyFile("phselectcity.py")
 spark.sparkContext.addPyFile("phsegwoot.py")
 spark.sparkContext.addPyFile("phcityoutliertemp.py")
+spark.sparkContext.addPyFile("photfactor.py")
 
 '''
     工作目录: 1.panel  2.universe  3.IMS v.s. MAX 
@@ -62,59 +58,37 @@ cities = df_cities.drop("key").toPandas()["city"].to_numpy()
 
 df_seg_city.persist()
 df_EIA_res.persist()
+df_seg_city.write.format("parquet") \
+    .mode("overwrite").save(u"hdfs://192.168.100.137/user/alfredyang/outlier/seg_city")
+df_EIA_res.write.format("parquet") \
+    .mode("overwrite").save(u"hdfs://192.168.100.137/user/alfredyang/outlier/EIA_res")
+
+cities = [u"北京市"]
+df_seg_city = spark.read.parquet(u"hdfs://192.168.100.137/user/alfredyang/outlier/seg_city")
+df_EIA_res = spark.read.parquet(u"hdfs://192.168.100.137/user/alfredyang/outlier/EIA_res")
 
 df_result = max_outlier_city_loop_template(spark, df_EIA_res, df_seg_city, cities)
+df_result.show()
 
 df_pnl = df_pnl.withColumnRenamed("City", "city") \
     .withColumnRenamed("Sales_pnl_mkt", "sales_pnl_mkt") \
     .withColumnRenamed("POI", "poi") \
     .withColumnRenamed("Sales_pnl", "sales_pnl")
-# df_pnl.show()
+df_pnl.show()
 
 df_result = df_result.join(df_pnl, on=["city", "poi"], how="left") \
     .join(df_ims_shr_res, on=["city", "poi"], how="left")
 
-df_result.show()
+df_result.write.format("parquet") \
+        .mode("overwrite").save(u"hdfs://192.168.100.137/user/alfredyang/outlier/result")
 
 # 调试Factor 流程
-'''
-    @fst_prd: 计算factor时，仅考虑前几个产品
-    @bias: 计算factor时，偏重于调准mkt的程度（数字越大越准）
-'''
-fst_prd = 3
-bias = 2
-prd_input = ["加罗宁", "凯纷", "诺扬"]
-for ct in cities:
-    print u"正在进行 %s factor的计算" % ct
-    df_rlt = df_result.where(df_result.city == ct)
-    rlt_scs = df_rlt.select("scen_id").distinct().toPandas()["scen_id"].to_numpy().tolist()
+# TODO: 可以直接从这里开始调试factor
+cities = [u"北京市"]
+df_result = spark.read.parquet(u"hdfs://192.168.100.137/user/alfredyang/outlier/result")
+[df_factor_result, df_rlt_brf] = max_outlier_factor(spark, df_result, cities)
 
-    for isc in rlt_scs:
-        df_rlt_sc = df_rlt.where(df_rlt.scen_id == isc).fillna(0.0)
-        df_rlt_sc.show()
-        # 一个线下算法库，没有替代品的情况下线下计算
-        rltsc = df_rlt_sc.toPandas()
-        f = Variable()
-        poi_ratio = {}
-        mkt_ratio = {}
-
-        for iprd in range(len(rltsc.index)):
-            poi_ratio[iprd] = np.divide(
-                (rltsc["poi_vol"][iprd] - rltsc["sales_pnl"][iprd]) * f + rltsc["sales_pnl"][iprd],
-                rltsc["ims_poi_vol"][iprd]) - 1
-            mkt_ratio[iprd] = np.divide(
-                (rltsc["mkt_vol"][iprd] - rltsc["sales_pnl_mkt"][iprd]) * f + rltsc["sales_pnl_mkt"][iprd],
-                rltsc["ims_mkt_vol"][iprd]) - 1
-
-        par = []
-        for s in range(len(rltsc.index)):
-            if rltsc["poi"][s] in prd_input[:fst_prd]:
-                par += ["np.divide(abs(poi_ratio[%s])," % s + str(bias) + ")"]
-                par += ["abs(mkt_ratio[%s])" % s]
-        exec ("obj=Minimize(max_elemwise(" + ",".join(par) + "))")
-        #        obj=Minimize(max_elemwise(abs(poi_ratio[0]),abs(poi_ratio[1]),abs(poi_ratio[2]),abs(poi_ratio[3]),
-        #                                  abs(mkt_ratio[0]),abs(mkt_ratio[1]),abs(mkt_ratio[2]),abs(mkt_ratio[3])))
-        prob = Problem(obj, [f > 0])
-        prob.solve()
-        print f
-        # df_rlt_sc = spark.createDataFrame(rltsc)
+df_factor_result.write.format("parquet") \
+    .mode("overwrite").save(u"hdfs://192.168.100.137/user/alfredyang/outlier/factor_result")
+df_rlt_brf.write.format("parquet") \
+    .mode("overwrite").save(u"hdfs://192.168.100.137/user/alfredyang/outlier/rlt_brf")
