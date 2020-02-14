f177f3289a3eaa0ee212400730439892108386a9
AlfredYang1986 <yangyuanpig@gmail.com>
2020-02-14 18:05:04 +0800

    试试 这个应该快

--- outerlier/photfactor.py
+++ outerlier/photfactor.py
@@ -4,6 +4,68 @@ import numpy as np
 from cvxpy import *
 from phOutlierParameters import prd_input,tmp_df_factor_result_path
 
+
+def max_outlier_factor_inner_loop_old(spark, rlt_scs, df_rlt, fst_prd, bias, index):
+    for isc in rlt_scs:
+        rltsc = df_rlt[df_rlt.scen_id == isc].fillna(0.0)
+        #df_rlt_sc.show()
+        # 一个线下算法库，没有替代品的情况下线下计算
+        # rltsc = df_rlt_sc.toPandas()
+        print (len(rltsc))
+        f = Variable()
+        poi_ratio = {}
+        mkt_ratio = {}
+
+        for iprd in range(len(rltsc.index)):
+            poi_ratio[iprd] = np.divide(
+                (rltsc["poi_vol"][iprd] - rltsc["sales_pnl"][iprd]) * f + rltsc["sales_pnl"][iprd],
+                rltsc["ims_poi_vol"][iprd]) - 1
+            mkt_ratio[iprd] = np.divide(
+                (rltsc["mkt_vol"][iprd] - rltsc["sales_pnl_mkt"][iprd]) * f + rltsc["sales_pnl_mkt"][iprd],
+                rltsc["ims_mkt_vol"][iprd]) - 1
+
+        par = []
+        for s in range(len(rltsc.index)):
+            if rltsc["poi"][s] in prd_input[:fst_prd]:
+                par += ["np.divide(abs(poi_ratio[%s])," % s + str(bias) + ")"]
+                par += ["abs(mkt_ratio[%s])" % s]
+        exec ("obj=Minimize(max_elemwise(" + ",".join(par) + "))")
+        # #        obj=Minimize(max_elemwise(abs(poi_ratio[0]),abs(poi_ratio[1]),abs(poi_ratio[2]),abs(poi_ratio[3]),
+        #                                  abs(mkt_ratio[0]),abs(mkt_ratio[1]),abs(mkt_ratio[2]),abs(mkt_ratio[3])))
+        prob = Problem(obj, [0 <= f])
+        prob.solve()
+        rltsc["factor"] = f.value
+        for i in range(len(rltsc)):
+            rltsc["scen"][i] = ",".join(rltsc["scen"][i])
+
+        df_tmp = spark.createDataFrame(rltsc)
+
+        #rltsc["scen"] = ",".join(rltsc["scen"])
+        print index
+        if(index == 0):
+            df_tmp.write.format("parquet") \
+                .mode("overwrite").save(tmp_df_factor_result_path)
+        else:
+            df_tmp.write.format("parquet") \
+                .mode("append").save(tmp_df_factor_result_path)
+            #df_result_all = df_result_all.union(df_result)
+        index = index + 1
+        #df_factor_result = df_factor_result.union(df_tmp)
+
+    return index
+
+
+def max_outlier_factor_cities_loop(spark, df_result, cities, fst_prd, bias):
+    index = 0
+    for ct in cities:
+        print (u"正在进行 %s factor的计算" % ct)
+        df_rlt = df_result.where(df_result.city == ct)
+        rlt_scs = df_rlt.select("scen_id").distinct().toPandas()["scen_id"].to_numpy().tolist()
+        index = max_outlier_factor_inner_loop_old(spark, rlt_scs, df_rlt.toPandas(), fst_prd, bias, index)
+
+    print (u"已完成 %s factor的计算" % ct)
+
+
 # 调试Factor 流程
 '''
     @fst_prd: 计算factor时，仅考虑前几个产品
@@ -31,63 +93,8 @@ def max_outlier_factor(spark, df_result, cities, fst_prd=3, bias=2):
     #     StructField("factor", DoubleType(), True),
     # ])
     # df_factor_result = spark.createDataFrame([], schema)
-    index = 0
-    for ct in cities:
-        print (u"正在进行 %s factor的计算" % ct)
-        df_rlt = df_result.where(df_result.city == ct)
-        rlt_scs = df_rlt.select("scen_id").distinct().toPandas()["scen_id"].to_numpy().tolist()
+    max_outlier_factor_cities_loop(spark, df_result, cities, fst_prd, bias)
 
-        for isc in rlt_scs:
-            df_rlt_sc = df_rlt.where(df_rlt.scen_id == isc).fillna(0.0)
-            #df_rlt_sc.show()
-            # 一个线下算法库，没有替代品的情况下线下计算
-            rltsc = df_rlt_sc.toPandas()
-            print (len(rltsc))
-            f = Variable()
-            poi_ratio = {}
-            mkt_ratio = {}
-
-            for iprd in range(len(rltsc.index)):
-                poi_ratio[iprd] = np.divide(
-                    (rltsc["poi_vol"][iprd] - rltsc["sales_pnl"][iprd]) * f + rltsc["sales_pnl"][iprd],
-                    rltsc["ims_poi_vol"][iprd]) - 1
-                mkt_ratio[iprd] = np.divide(
-                    (rltsc["mkt_vol"][iprd] - rltsc["sales_pnl_mkt"][iprd]) * f + rltsc["sales_pnl_mkt"][iprd],
-                    rltsc["ims_mkt_vol"][iprd]) - 1
-
-            par = []
-            for s in range(len(rltsc.index)):
-                if rltsc["poi"][s] in prd_input[:fst_prd]:
-                    par += ["np.divide(abs(poi_ratio[%s])," % s + str(bias) + ")"]
-                    par += ["abs(mkt_ratio[%s])" % s]
-            exec ("obj=Minimize(max_elemwise(" + ",".join(par) + "))")
-            # #        obj=Minimize(max_elemwise(abs(poi_ratio[0]),abs(poi_ratio[1]),abs(poi_ratio[2]),abs(poi_ratio[3]),
-            #                                  abs(mkt_ratio[0]),abs(mkt_ratio[1]),abs(mkt_ratio[2]),abs(mkt_ratio[3])))
-            prob = Problem(obj, [0 <= f])
-            prob.solve()
-            rltsc["factor"] = f.value
-            for i in range(len(rltsc)):
-                rltsc["scen"][i] = ",".join(rltsc["scen"][i])
-
-            df_tmp = spark.createDataFrame(rltsc)
-
-            #rltsc["scen"] = ",".join(rltsc["scen"])
-            print index
-            if(index == 0):
-                df_tmp.write.format("parquet") \
-                    .mode("overwrite").save(tmp_df_factor_result_path)
-            else:
-                df_tmp.write.format("parquet") \
-                    .mode("append").save(tmp_df_factor_result_path)
-                #df_result_all = df_result_all.union(df_result)
-            index = index + 1
-
-
-
-
-            #df_factor_result = df_factor_result.union(df_tmp)
-
-        print (u"已完成 %s factor的计算" % ct)
     df_factor_result = spark.read.parquet(tmp_df_factor_result_path)
     df_factor_result = df_factor_result.withColumn("poi_tmp",
                                                    ((df_factor_result.poi_vol - df_factor_result.sales_pnl) *
